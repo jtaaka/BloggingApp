7d28722cdeadb439faeef7e91302e820ec816804
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-03-19 22:02:52 +0200

    Update src/components/login/login.jsx
    Login UI change

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -7,18 +7,19 @@ class Login extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {username: "", password: ""};
+    this.state = {username: "ad", password: "", loggedIn: false};
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.validateForm = this.validateForm.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   validateForm() {
     return this.state.username.match(/^[a-z0-9\-_.]+$/i) !== null
-      && this.state.username.length > 0
-      && this.state.username.length < 20
-      && this.state.password.length > 0;
+        && this.state.username.length > 0
+        && this.state.username.length < 20
+        && this.state.password.length > 0;
   }
 
   handleChange = event => {
@@ -29,6 +30,8 @@ class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    let status = false;
 
     console.log("user: " + this.state.username);
     console.log("pass: " + this.state.password);
@@ -43,32 +46,53 @@ class Login extends Component {
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(requestBody)
     })
-      .then(function(response) {
-        if(response.status === 200) {
-          console.log("SUCCESS");
-        } else if(response.status === 401) {
-          alert("Invalid username or password");
-        }
-      })
+        .then(function(response) {
+          if(response.status === 200) {
+            console.log("SUCCESS");
+            status = true;
+          } else if(response.status === 401) {
+            alert("Invalid username or password");
+            status = false;
+          }
+        }).then(() => { if(status) {
+        this.setState({loggedIn: true})
+        } else {
+          this.setState({loggedIn: false})}});
+  }
+
+  logout() {
+    this.setState({loggedIn: false})
   }
 
   render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-      <InputGroup>
-        <FormControl id="username" onChange={this.handleChange} placeholder="Enter username" className="mr-sm-2"/>
-        <FormControl id="password" onChange={this.handleChange} placeholder="Enter password"/>
-        <InputGroup.Append>
-          <Button
-            variant ="primary"
-            disabled={!this.validateForm()}
-            type="submit">
-            Log in
-          </Button>
-        </InputGroup.Append>
-      </InputGroup>
-      </Form>
-    );
+
+    if (this.state.loggedIn === false) {
+      return (
+          <Form onSubmit={this.handleSubmit}>
+            <InputGroup>
+              <FormControl id="username" onChange={this.handleChange} placeholder="Enter username" className="mr-sm-2"/>
+              <FormControl id="password" onChange={this.handleChange} placeholder="Enter password"/>
+              <InputGroup.Append>
+                <Button
+                    variant="primary"
+                    disabled={!this.validateForm()}
+                    type="submit">
+                  Log in
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+      );
+    } else {
+      return (
+          <InputGroup>
+            <InputGroup.Text>Logged In as, {this.state.username}</InputGroup.Text>
+            <InputGroup.Append>
+              <Button onClick={this.logout}>Log Out</Button>
+            </InputGroup.Append>
+          </InputGroup>
+      );
+    }
   }
 }
 
