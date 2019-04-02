598844b166d91370ad80482914c0f57261b3c05f
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-02 21:22:28 +0300

    Create add_blog.jsx
    Add new blogpost functionality

---
+++ src/components/add_blog/add_blog.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Button, Form } from "react-bootstrap";
+import * as ReactDOM from "react-dom";
+
+const URL = "http://localhost:8080/posts";
+
+class Add_blog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      content: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.add = this.add.bind(this);
+  }
+
+  handleChange = event => {
+    event.preventDefault();
+
+    this.setState({[event.target.id]: event.target.value});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    let requestBody = {
+      title: this.state.title,
+      content: this.state.content
+    };
+
+    fetch(URL, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(requestBody)
+    }).then(() => console.log(this.state.title + " " + this.state.content))
+  }
+
+  add() {
+    ReactDOM.render(
+    <div className="mx-auto">
+      <Form.Label>Title</Form.Label>
+      <Form.Control id="title" onChange={this.handleChange} placeholder="Enter title" className="mr-sm-2"/>
+      <Form.Label>Content</Form.Label>
+      <Form.Control id="content" as="textarea" rows="5" placeholder="Enter text" onChange={this.handleChange}/>
+      <Button className="btn btn-primary float-right" onClick={this.handleSubmit}>Submit</Button>
+    </div>
+    , document.getElementById("content"))
+  }
+
+  render() {
+      return (
+          <Button onClick={this.add}>Add new blogpost</Button>
+      );
+  }
+}
+
+export default Add_blog;
