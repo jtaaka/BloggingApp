74fbc0f30f606d98fd2ce5fe03301a5fcc771c75
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-03 16:39:33 +0300

    Update search.jsx
    Search function modification

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -4,8 +4,11 @@ import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from "react-bootstrap/InputGroup";
 import {Card} from "react-bootstrap";
 import * as ReactDOM from "react-dom";
+import Editpost from "../editpost/editpost";
+import Blogposts from "../blogposts/blogposts";
 
 const URL = "http://localhost:8080/posts";
+const DELETE_URL = "http://localhost:8080/posts/";
 
 class Search extends Component {
   constructor(props) {
@@ -14,6 +17,10 @@ class Search extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.search = this.search.bind(this);
+    this.delete = this.delete.bind(this);
+    this.editPost = this.editPost.bind(this);
+    this.renderSearch = this.renderSearch.bind(this);
+    this.renderMain = this.renderMain.bind(this);
   }
 
   componentDidMount() {
@@ -25,6 +32,27 @@ class Search extends Component {
         .then(data => this.setState({ blogposts: data }));
   }
 
+  delete(id) {
+    fetch(DELETE_URL + id, {
+      method: 'DELETE',//
+      headers: {'Content-Type': 'application/json'},
+    }).then(() => console.log("Deleted blogpost id = " + id)).then(() =>
+        fetch(URL, {
+          method: 'GET',
+          headers: {'Content-Type': 'application/json'},
+        })
+            .then(response => response.json())
+            .then(data => this.setState({ blogposts: data })).then(() => this.renderSearch()))
+  }
+
+  editPost(id, title, content) {
+    console.log(id, title, content)
+
+    ReactDOM.render(
+        <Editpost id={id} title={title} content={content}/>
+        , document.getElementById("content"))
+  };
+
   handleChange = event => {
     event.preventDefault();
 
@@ -32,24 +60,47 @@ class Search extends Component {
   }
 
   search() {
+    fetch(URL, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+        .then(response => response.json())
+        .then(data => this.setState({ blogposts: data })).then(() => this.renderSearch())
+  }
+
+  renderSearch() {
     let content = [];
 
-    { this.state.blogposts.map( post => {
-      if (post.title.toLowerCase().includes(this.state.filter)) {
-        content.push(
-        <Card border="dark" style={{ width: '60%', margin: '0 auto', marginTop: "25px" }}>
-          <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
-            <Card.Text>
-              {post.content}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Posted at {post.date}</small>
-          </Card.Footer>
-        </Card>)
-      }
-    })
+    {
+      this.state.blogposts.map(post => {
+        if (post.title.toLowerCase().includes(this.state.filter)) {
+          content.push(
+              <Card border="dark" style={{width: '60%', margin: '0 auto', marginTop: "25px"}}>
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Text>
+                    {post.content}
+                  </Card.Text>
+                  <Button className="btn float-left" id={post.id}
+                          onClick={(e) => this.editPost(e.target.id, post.title, post.content)}
+                          variant="secondary">Edit</Button>
+                  <Button className="btn btn-primary float-right" id={post.id}
+                          onClick={(e) => this.delete(e.target.id)}>Delete</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Posted at {post.date}</small>
+                </Card.Footer>
+              </Card>)
+        }
+      })
+    }
+
+    if (content.length > 0) {
+      content.push(
+          <Card border="dark" style={{ width: '60%', margin: '0 auto', marginTop: "25px" }}>
+            <Button className="btn btn-primary" id="backToMain" onClick={this.renderMain}>Back to all blogs</Button>
+          </Card>
+      )
     }
 
     if (content.length < 1) {
@@ -61,11 +112,18 @@ class Search extends Component {
                 Try another one!
               </Card.Text>
             </Card.Body>
+            <Button className="btn btn-primary" id="backToMain" onClick={this.renderMain}>Back to all blogs</Button>
           </Card>
       )
     }
 
     ReactDOM.render(<div className="card text-center">{content}</div>, document.getElementById("content"))
+  }
+
+  renderMain() {
+    ReactDOM.render(
+        <Blogposts/>
+        , document.getElementById("content"))
   }
 
   render() {
