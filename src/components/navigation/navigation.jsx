e8d197b4e8b1950488f4239df87985f8e763150d
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-24 20:11:27 +0300

    Rework Navigation component

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,31 +1,80 @@
-import React, { Component } from "react";
-import { Navbar, Nav, Button } from 'react-bootstrap';
-import Login from '../login/login'
-import { NavLink } from 'react-router-dom'
+import React, {Component} from "react";
+import {Navbar, Nav, Button} from 'react-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import Cookies from "js-cookie";
+import {Redirect, withRouter} from 'react-router-dom';
+import './navigation.css';
 
-class Navigation extends Component {
+export class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+      logout: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.logout = this.logout.bind(this);
+    this.renderLogout = this.renderLogout.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  logout() {
+    Cookies.remove("username");
+    Cookies.remove("loggedIn");
+    this.setState({"logout":true});
+  };
+
+  renderLogout(){
+    if(this.state.logout) {
+      this.setState({"logout": false});
+      return (<Redirect to="/login"/>);
+    }
+  }
+
+  handleSelect(eventKey) {
+    console.log({eventKey})
+  }
 
   render() {
-    return (
-      <div>
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" sticky="top">
-          <NavLink exact to="/">
-            <Navbar.Brand>
-              BlogApp
-            </Navbar.Brand>
-          </NavLink>
-          <Navbar.Toggle/>
-          <Navbar.Collapse>
-            <Nav>
-              <Login/>
-            </Nav>
-            <Nav className="mr-auto">
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    )
+    if (window.location.pathname === '/login' || window.location.pathname === '/any') {
+      return null;
+    } else {
+      return (
+          <div>
+            {this.renderLogout()}
+            <Navbar expand="md" bg="dark" variant="dark" collapseOnSelect="true">
+
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                  Blogging app
+                </Navbar.Brand>
+              </LinkContainer>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+              <Navbar.Collapse id="responsive-navbar-nav">
+
+                <Nav id ="borderless" variant="tabs" className="mr-auto" activeKey="1" onSelect={k => this.handleSelect(k)}>
+                  <LinkContainer to={"/addpost"} exact={true}>
+                    <Nav.Link>Add Blogpost</Nav.Link>
+                  </LinkContainer>
+                </Nav>
+
+                <Navbar.Text id="signed-in">Signed in as, {Cookies.get("username")}</Navbar.Text>
+                <Nav.Item>
+                  <Button id="logout" variant="outline-danger" onClick={this.logout}>Log out</Button>
+                </Nav.Item>
+              </Navbar.Collapse>
+            </Navbar>
+          </div>
+      );
+    }
   }
 }
 
-export default Navigation;
+export default withRouter(Navigation);
